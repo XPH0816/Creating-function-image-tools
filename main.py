6886f2230b4c5fa1cd65311ef57e8ec1f9953e3e
XPH0904 <50739818+XPH0904@users.noreply.github.com>
2021-02-02 18:37:16 +0800

    adding image, record point to point position

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 from PIL import Image, ImageDraw, ImageFont
 
 # Creating an image with 600px * 600px and a white background
-img = Image.new('RGBA', (600, 600), (255, 255, 255,255))
+img = Image.new('RGBA', (600, 600), (255, 255, 255, 255))
+img_arrow = Image.open('image\line with middle arrow.png').convert('RGBA')
 
 # Creating Draw Tools on the img and img_text
 draw = ImageDraw.Draw(img)
@@ -10,12 +11,36 @@ draw = ImageDraw.Draw(img)
 # Get the img size
 x,y = img.size
 
+# Constant position to arrange the range of the function
+CONST = 0.25
+
 # Set the position value
 left_x1,left_y1 = (100,100)
 left_x2,left_y2 = (100,550)
 
 right_x1,right_y1 = (x-left_x1, left_y1)
 right_x2,right_y2 = (x-left_x2, left_y2)
+
+left_text_x = (left_x1-15)
+left_text_y = (left_y1-50)
+
+right_text_x = (right_x1-15)
+right_text_y = (right_y1-50)
+
+function_x = (x/2)
+function_y = (left_y1-60)
+
+domain_x = left_x1-40
+domain_y = (left_y1+left_y2)/6
+
+range_x = right_x1+20
+range_y = (right_y1+right_y2)/6
+
+mapping_line_x1, mapping_line_y1 = (domain_x+40, domain_y)
+mapping_line_x2, mapping_line_y2 = (range_x-20, range_y)
+
+label_line_x1, label_line_x2 = (mapping_line_x1-10, mapping_line_x1+10)
+label_line_x3, label_line_x4 = (mapping_line_x2-10, mapping_line_x2+10)
 
 # Get the font 
 fnt = ImageFont.truetype('font\FreeSerif.ttf', 44)
@@ -31,15 +56,84 @@ draw.line((left_x1, left_y1, left_x2, left_y2), fill=(0, 0, 0), width=3)
 draw.line((right_x1, right_y1, right_x2, right_y2), fill=(0, 0, 0), width=3)
 
 # Place the text
-draw.text((left_x1-15, left_y1-50), "A", font=fnt, fill=(0, 0, 0, 255))
-draw.text((right_x1-15, right_y1-50), "B", font=fnt, fill=(0, 0, 0, 255))
-draw.text((x/2, left_y1-60), "f", font=fnt_i, fill=(0, 0, 0, 255))
+draw.text((left_text_x, left_text_y), "A", font=fnt, fill=(0, 0, 0, 255))
+draw.text((right_text_x, right_text_y), "B", font=fnt, fill=(0, 0, 0, 255))
+draw.text((function_x, function_y), "f", font=fnt_i, fill=(0, 0, 0, 255))
 
-#Creating Domain
+# Creating Domain
+draw.text((domain_x, domain_y*(1)), "1", font=fnt, fill=(0, 0, 0, 255))
+draw.text((domain_x, domain_y*(2)), "b", font=fnt_i, fill=(0, 0, 0, 255))
+draw.text((domain_x, domain_y*(3)), "3", font=fnt, fill=(0, 0, 0, 255))
+draw.text((domain_x, domain_y*(4)), "4", font=fnt, fill=(0, 0, 0, 255))
+
+# Draw Domain Label Line 
+draw.line((label_line_x1, domain_y*(1.25), label_line_x2, domain_y*(1.25)), fill=(0, 0, 0), width=2)
+draw.line((label_line_x1, domain_y*(2.25), label_line_x2, domain_y*(2.25)), fill=(0, 0, 0), width=2)
+draw.line((label_line_x1, domain_y*(3.25), label_line_x2, domain_y*(3.25)), fill=(0, 0, 0), width=2)
+draw.line((label_line_x1, domain_y*(4.25), label_line_x2, domain_y*(4.25)), fill=(0, 0, 0), width=2)
+
+# Creating Range
+draw.text((range_x, range_y*(1+CONST)), "a", font=fnt_i, fill=(0, 0, 0, 255))
+draw.text((range_x, range_y*(2+CONST/2)), "2", font=fnt, fill=(0, 0, 0, 255))
+draw.text((range_x, range_y*(3-CONST/2)), "c", font=fnt_i, fill=(0, 0, 0, 255))
+draw.text((range_x, range_y*(4-CONST)), "d", font=fnt_i, fill=(0, 0, 0, 255))
+
+# Draw Range Label Line 
+draw.line((label_line_x3, range_y*(1.25+CONST), label_line_x4, range_y*(1.25+CONST)), fill=(0, 0, 0), width=2)
+draw.line((label_line_x3, range_y*(2.25+CONST/2), label_line_x4, range_y*(2.25+CONST/2)), fill=(0, 0, 0), width=2)
+draw.line((label_line_x3, range_y*(3.25-CONST/2), label_line_x4, range_y*(3.25-CONST/2)), fill=(0, 0, 0), width=2)
+draw.line((label_line_x3, range_y*(4.25-CONST), label_line_x4, range_y*(4.25-CONST)), fill=(0, 0, 0), width=2)
+
+# Draw Mapping line
+#draw.line((mapping_line_x1, mapping_line_y1*(1.25), mapping_line_x2, mapping_line_y2*(1.25+CONST)), fill=(0, 0, 0), width=4)
+#draw.line((mapping_line_x1, mapping_line_y1*(2.25), mapping_line_x2, mapping_line_y2*(2.25+CONST/2)), fill=(0, 0, 0), width=4)
+#draw.line((mapping_line_x1, mapping_line_y1*(3.25), mapping_line_x2, mapping_line_y2*(3.25-CONST/2)), fill=(0, 0, 0), width=4)
+#draw.line((mapping_line_x1, mapping_line_y1*(4.25), mapping_line_x2, mapping_line_y2*(4.25-CONST)), fill=(0, 0, 0), width=4)
+
+# Draw the arrow to the line
+# 1 to 1
+#img_arrow = img_arrow.rotate(-4.5, expand=True)
+#img_arrow = img_arrow.resize((round(img_arrow.size[0]*0.85), round(img_arrow.size[1]*0.75)))
+#img.paste(img_arrow, (90, int(domain_y*(1.15))), img_arrow)
+
+# 1 to 2
+#img_arrow = img_arrow.rotate(-20, expand=True)
+#img_arrow = img_arrow.resize((round(img_arrow.size[0]*0.9), round(img_arrow.size[1]*0.75)))
+#img.paste(img_arrow, (90, int(domain_y*(1.15))), img_arrow)
+
+# 1 to 3
+#img_arrow = img_arrow.rotate(-35.5, expand=True)
+#img_arrow = img_arrow.resize((round(img_arrow.size[0]*1.05), round(img_arrow.size[1]*0.75)))
+#img.paste(img_arrow, (90, int(domain_y*(1.15))), img_arrow)
+
+# 1 to 4
+#img_arrow = img_arrow.rotate(-57, expand=True)
+#img_arrow = img_arrow.resize((round(img_arrow.size[0]*1.55), round(img_arrow.size[1]*0.75)))
+#img.paste(img_arrow, (80, int(domain_y*(1.2))), img_arrow)
+
+# 2 to 1
+#img_arrow = img_arrow.rotate(13.2, expand=True)
+#img_arrow = img_arrow.resize((round(img_arrow.size[0]*0.88), round(img_arrow.size[1]*0.75)))
+#img.paste(img_arrow, (95, int(domain_y*(1.39))), img_arrow)
+
+# 2 to 2
+#img_arrow = img_arrow.rotate(-2.2, expand=True)
+#img_arrow = img_arrow.resize((round(img_arrow.size[0]*0.88), round(img_arrow.size[1]*0.75)))
+#img.paste(img_arrow, (95, int(domain_y*(2.15))), img_arrow)
+
+# 2 to 3
+#img_arrow = img_arrow.rotate(-15.4, expand=True)
+#img_arrow = img_arrow.resize((round(img_arrow.size[0]*0.88), round(img_arrow.size[1]*0.75)))
+#img.paste(img_arrow, (95, int(domain_y*(2.15))), img_arrow)
+
+# 2 to 4
+#img_arrow = img_arrow.rotate(-32.2, expand=True)
+#img_arrow = img_arrow.resize((round(img_arrow.size[0]*1), round(img_arrow.size[1]*0.75)))
+#img.paste(img_arrow, (90, int(domain_y*(2.17))), img_arrow)
 
 
-#Show image
+# Show image
 img.show()
 
-#Save image
+# Save image
 #img.save("./image/test.png")
